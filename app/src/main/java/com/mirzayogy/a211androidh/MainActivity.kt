4484d5c7adfc3f8e02462a950bc839bf02369386
mirzayogy <mirza.yogy@gmail.com>
2021-11-05 09:20:36 +0800

    EditText, val dan var

--- app/src/main/java/com/mirzayogy/a211androidh/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/a211androidh/MainActivity.kt
@@ -13,10 +13,19 @@ class MainActivity : AppCompatActivity() {
 
         val simpanButton = findViewById<Button>(R.id.simpanButton)
         val batalButton = findViewById<Button>(R.id.batalButton)
+
         val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val noHandphoneEditText = findViewById<EditText>(R.id.noHandphoneEditText)
+        val jarakEditText = findViewById<EditText>(R.id.jarakEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
 
         simpanButton.setOnClickListener {
-            Toast.makeText(this,"Hai ",Toast.LENGTH_SHORT).show()
+            val nama = namaEditText.text.toString()
+            var jarak = jarakEditText.text.toString().toInt()
+            jarak = jarak * 1000
+
+            Toast.makeText(this,"Hai $jarak",Toast.LENGTH_SHORT).show()
         }
 
         batalButton.setOnClickListener {
